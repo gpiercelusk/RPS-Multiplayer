1b5b5a40eed1f55dc95bfe1939040c2b5083eb3d
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-19 14:18:40 -0400

    finished layout of the came and some styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyDFI9bgpwNf2PJng4NLeo9S8F3Hq_Po-Dc",
+  authDomain: "rps-multiplayer-a9d84.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-a9d84.firebaseio.com",
+  projectId: "rps-multiplayer-a9d84",
+  storageBucket: "rps-multiplayer-a9d84.appspot.com",
+  messagingSenderId: "958621680901"
+};
+firebase.initializeApp(config);
